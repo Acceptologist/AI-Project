c1733f8977e209e858bbfa03105080c2fbeac1f1
Mohamed Sayed <mohasaad1696@gmail.com>
2018-04-17 15:13:23 +0200

    Update 8_Queen Problem.py

--- 8_Queen/8_Queen Problem.py
+++ 8_Queen/8_Queen Problem.py
@@ -1,151 +1,95 @@
-from copy import deepcopy
+from random import *
 
-
-goal = []
-Visited = []
 Path = []
+chess = [[0 for i in range(0, 8, 1)] for j in range(0, 8, 1)]
 
 
-def issolvable(puzzle):
-    count = 0
-    for i in range(len(puzzle)):
-        for j in range(i+1, len(puzzle), 1):
-            if puzzle[i] > puzzle[j]:
-                count += 1
-    return count % 2 == 0
+############################################################################
 
 
-def isgoal(puzzle):
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] != goal[i][j]:
-                return False
+def issafe(row, col):
+
+    for i in range(0, 8, 1):
+        if chess[i][col] == 1:
+            return False
+
+    i = row-1
+    j = col-1
+    while i > -1 and j > -1:
+        if chess[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    i = row - 1
+    j = col + 1
+    while i > -1 and j < 8:
+        if chess[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+
     return True
 
 
-def isvalid(i, j, old_puzzle, zero):
-    puzzle = deepcopy(old_puzzle)
-    if -1 < i < 3 and -1 < j < 3:
-        puzzle[zero[0]][zero[1]] = puzzle[i][j]
-        puzzle[i][j] = 0
-        if puzzle not in Visited:
-            return True
+############################################################################
+
+
+def solve(row):
+    if row == 7:
+        for j in range(0, 8, 1):
+            if issafe(row, j):
+                Path.append([row, j])
+                return True
+        return False
+
+    for j in range(0, 8, 1):
+        if issafe(row, j):
+            chess[row][j] = 1
+            Path.append([row, j])
+
+            if solve(row+1):
+                return True
+
+            Path.pop(-1)
+            chess[row][j] = 0
     return False
 
 
-######################################################
-def hamming(i, j, old_puzzle, zero):
-    puzzle = deepcopy(old_puzzle)
-    puzzle[zero[0]][zero[1]] = puzzle[i][j]
-    puzzle[i][j] = 0
-    steps = 0
-
-    for I in range(3):
-        for J in range(3):
-            if puzzle[I][J] != goal[I][J]:
-                steps += 1
-    return steps
+############################################################################
 
 
-def findzero(puzzle):
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] == 0:
-                return i, j
-    return 0, 0
+def printpath(chess):
 
-
-def changepuzzle(puzzle, node, zero):
-    puzzle[zero[0]][zero[1]] = puzzle[node[0]][node[1]]
-    puzzle[node[0]][node[1]] = 0
-    return puzzle
-
-
-def sort(solutions):
-    for i in range(len(solutions)):
-        for j in range(i+1, len(solutions)):
-            if solutions[i][0] > solutions[j][0]:
-                solutions[i], solutions[j] = solutions[j], solutions[i]
-
-
-######################################################
-def solve(puzzle):
-
-    Path.append(deepcopy(puzzle))
-
-    while not isgoal(puzzle):
-
-        solutions = []
-        if puzzle not in Visited:
-            Visited.append(deepcopy(puzzle))
-        zero = findzero(puzzle)
-
-        if isvalid(zero[0]-1, zero[1], puzzle, zero):
-            solutions.append([hamming(zero[0]-1, zero[1], puzzle, zero), [zero[0]-1, zero[1]]])
-
-        if isvalid(zero[0]+1, zero[1], puzzle, zero):
-            solutions.append([hamming(zero[0]+1, zero[1], puzzle, zero), [zero[0]+1, zero[1]]])
-
-        if isvalid(zero[0], zero[1]-1, puzzle, zero):
-            solutions.append([hamming(zero[0], zero[1]-1, puzzle, zero), [zero[0], zero[1]-1]])
-
-        if isvalid(zero[0], zero[1]+1, puzzle, zero):
-            solutions.append([hamming(zero[0], zero[1]+1, puzzle, zero), [zero[0], zero[1]+1]])
-
-        sort(solutions)
-
-        if len(solutions) == 0:
-            return False
-
-        else:
-            node = solutions[0][1]
-            puzzle = changepuzzle(puzzle, node, zero)
-            Path.append(deepcopy(puzzle))
-    return True
-
-
-def tracepath():
-
-    with open('8_Puzzle_Output.txt', 'w') as f:
-        f.write("\nThe Solution Of The Puzzle  :\n")
+    with open("8_Quene", 'w') as f:
+        f.write("The Solution :\n\n----------------------------------------- \n\n")
         string = ""
-        for i in range(len(Path)):
-            if i != 0:
-                string += "The Next Step ::: ==>>\n"
-            for j in range(3):
+
+        for i in range(0, 8, 1):
+            for j in range(0, 8, 1):
+                chess[i][j] = 0
+                string += "0  "
+            string += '\n'
+        f.write(string+"\n----------------------------------------- \n\n")
+
+        for i in range(0, 8, 1):
+            chess[Path[i][0]][Path[i][1]] = 1
+            string = "The Next Step ::: ==>>\n\n"
+            for i in range(0, 8, 1):
+                for j in range(0, 8, 1):
+                    string += str(chess[i][j]) + "  "
                 string += '\n'
-                for k in range(3):
-                    string += str(Path[i][j][k])+' '
-                string += '\n'
-            if i != len(Path)-1:
-                string += "\n----------------------------------------- \n"
-
-        f.write(string+"\n\nMinimum Number Of Moves = "+str(len(Path)-1))
+            f.write(string+"\n----------------------------------------- \n\n")
 
 
-def GO():
-    puzzle = []
-    test = []
-    with open("Puzzle.txt", 'r') as f:
-        for line in f:
-            puzzle.append([int(line[0]), int(line[2]), int(line[4])])
-
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] != 0:
-                test.append(puzzle[i][j])
-
-    goal.append([1, 2, 3])
-    goal.append([4, 5, 6])
-    goal.append([7, 8, 0])
-
-    if issolvable(test):
-        if solve(puzzle):
-            tracepath()
-        else:
-            print("Can't Find Solution")
-    else:
-        print("This Puzzle is Unsolvable")
+############################################################################
 
 
-GO()
+column = randint(0, 7)
+chess[0][column] = 1
+Path.append([0, column])
+
+if solve(1):
+    printpath(chess)
+else:
+    print "Solution Does Not Exsists"
